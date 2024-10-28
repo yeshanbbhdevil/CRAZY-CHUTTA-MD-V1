2c4db19e06f67362bdb4fff45354bbdc6bad1bb1
CRAZY-CHUTTA-MD <chuttacrazy@gmail.com>
2024-10-28 12:57:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMAGE: process.env.ALIVE_IMG || "https://i.ibb.co/Pcw5MZq/In-Collage-20241028-103516143.jpg",
+AUTO_READ_STATUS: process.evn.AUTO_READ_STATUS || "true"
 };
