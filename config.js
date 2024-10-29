712097b80bc399a83f61b941e691207e266d23de
yeshanbbhdevil <yeshanbasnayake20071001@gmail.com>
2024-10-29 08:12:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "g6tHSDQL#lbCwmX4jLj4I8xVkfVDhx75aMazNUcm0XdKBKfJP4UM",
 ALIVE_IMAGE: process.env.ALIVE_IMG || "https://i.ibb.co/Pcw5MZq/In-Collage-20241028-103516143.jpg",
 AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
 };
